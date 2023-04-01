f7f91e6eb4d62db87da09db6358396d9a2063505
serkankaragoz <serkankaragoz2002@gmail.com>
2023-04-01 13:10:39 +0300

    Updated readme.md file

--- components/footer/index.js
+++ components/footer/index.js
@@ -3,8 +3,10 @@ import styles from "./styles.module.css";
 
 function Footer() {
   return (
-    <div></div>
-    /*<footer className={styles.footer}>This is Footer</footer>*/
+    
+      <footer className={styles.footer}>This is Footer</footer>
+   
+    
   )
 }
 
--- pages/layout.jsx
+++ pages/layout.jsx
@@ -11,9 +11,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Header/>
-      <main>{children}</main> 
-      <Footer/>
+      
+      <main>
+        {children}
+      </main> 
+     
     </html>
   )
 }
